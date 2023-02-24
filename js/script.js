54ee74e660e45517abbd4263b0324ea063cfb2cf
domenicobacoli <domenico.bacoli89@gmail.com>
2023-02-24 16:18:36 +0100

    feat: add for loop with condition + div element create in js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi
 “FizzBuzz”.
 */
 
+// - loop for numeri da 1 a 100 in console.log
+//   ? SE il numero ottenuto è un multiplo di 3
+//      ° stampa in console "Fizz"
+//   :? SE il numero ottenuto è un multiplo di 5
+//      ° stampa in console "Buzz"
+//   :? SE è sia multiplo di 3 che di 5
+//      ° stampa in console "FizzBuzz"
+
+let containerEl = document.querySelector(".container");
+
+for(let i = 1; i < 100; i++){
+
+    let newEl = document.createElement("div");
+    newEl.innerHTML = i
+    containerEl.append(newEl);
+    
+    if (( i % 3 == 0 ) && ( i % 5 == 0 )){
+        newEl.innerHTML = "FizzBuzz"
+
+    } else if ( i % 3 == 0 ){
+        newEl.innerHTML = "Fizz";
+
+    } else if ( i % 5 == 0 ){
+        newEl.innerHTML = "Buzz";
+    }
+}
